574a766451cab21b058fd9cbcbf5df45d72ea32c
Justin Singleton <38257985+JustinSingleton303@users.noreply.github.com>
2021-02-20 16:22:19 -0700

    sounds mapped to buttons

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 alert("script file linked");
 
-document.querySelector(".w").addEventListener("click", w);
 
-function w(url){
+/* adding event listeners for all of our buttons */
+document.querySelector(".w").addEventListener("click", sound);
+document.querySelector(".a").addEventListener("click", sound);
+document.querySelector(".s").addEventListener("click", sound);
+document.querySelector(".d").addEventListener("click", sound);
+document.querySelector(".j").addEventListener("click", sound);
+document.querySelector(".k").addEventListener("click", sound);
+document.querySelector(".l").addEventListener("click", sound);
+
+function sound(url){
   new Audio(url).play();
 }
