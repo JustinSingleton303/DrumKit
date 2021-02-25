adc518df423906762e583bf848303d8a445ff581
Justin Singleton <38257985+JustinSingleton303@users.noreply.github.com>
2021-02-25 08:04:01 -0700

    drum kit sounds play now with keyboard events

--- index.js
+++ index.js
@@ -9,11 +9,39 @@ var soundArray = ["sounds/snare.mp3", "sounds/kick-bass.mp3",
 
 var keyArray = ["w", "a", "s", "d", "j", "k", "l"];
 
-/* upper index bound for drums */
+/* upper index bound for drums and keys*/
 var numDrums = document.querySelectorAll(".drum").length;
+var numKey = keyArray.length;
 
-document.addEventListener("keydown", function() {
-  alert("key pressed");
+// making the sounds availible through the keyboard
+document.addEventListener("keydown", function(event) {
+  var aKey = event.key;
+
+  switch(aKey){
+    case "w":
+      sound(soundArray[0]);
+      break;
+    case "a":
+      sound(soundArray[1]);
+      break;
+    case "s":
+      sound(soundArray[2]);
+      break;
+    case "d":
+      sound(soundArray[3]);
+      break;
+    case "j":
+      sound(soundArray[4]);
+      break;
+    case "k":
+      sound(soundArray[5]);
+      break;
+    case "l":
+      sound(soundArray[6]);
+      break;
+    default:
+      break;
+  }
 });
 
 for (var i = 0; i < numDrums; i++) {
