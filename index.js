598ffec69dcb15b9423de99af3e73fe5c7b2ad88
Justin Singleton <38257985+JustinSingleton303@users.noreply.github.com>
2021-02-24 19:17:10 -0700

    keyboard event added

--- index.js
+++ index.js
@@ -2,30 +2,27 @@
   Author: Justin Singleton */
 
 var soundArray = ["sounds/snare.mp3", "sounds/kick-bass.mp3",
-                  "sounds/tom-1.mp3", "sounds/tom-2.mp3",
-                  "sounds/tom-3.mp3", "sounds/tom-4.mp3",
-                  "sounds/crash.mp3"];
-                  
+  "sounds/tom-1.mp3", "sounds/tom-2.mp3",
+  "sounds/tom-3.mp3", "sounds/tom-4.mp3",
+  "sounds/crash.mp3"
+];
+
+var keyArray = ["w", "a", "s", "d", "j", "k", "l"];
+
 /* upper index bound for drums */
 var numDrums = document.querySelectorAll(".drum").length;
 
-for(var i = 0; i < numDrums; i++){
-  document.querySelectorAll(".drum").addEventListener("click", function(){
+document.addEventListener("keydown", function() {
+  alert("key pressed");
+});
+
+for (var i = 0; i < numDrums; i++) {
+  document.querySelectorAll(".drum").addEventListener("click", function() {
     sound(soundArray[i]);
 
   });
 }
 
-/* adding event listeners for all of our buttons */
-/*
-document.querySelector(".w").addEventListener("click", sound);
-document.querySelector(".a").addEventListener("click", sound);
-document.querySelector(".s").addEventListener("click", sound);
-document.querySelector(".d").addEventListener("click", sound);
-document.querySelector(".j").addEventListener("click", sound);
-document.querySelector(".k").addEventListener("click", sound);
-document.querySelector(".l").addEventListener("click", sound);*/
-
-function sound(url){
+function sound(url) {
   new Audio(url).play();
 }
