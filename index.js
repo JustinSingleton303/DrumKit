32001e5c21784a6abb2a7307f1593c9605d50bc3
Justin Singleton <38257985+JustinSingleton303@users.noreply.github.com>
2021-02-28 07:34:29 -0700

    animations completed

--- index.js
+++ index.js
@@ -67,4 +67,7 @@ function sound(url) {
 function buttonAnimation(currentKey){
   var activeButton = document.querySelector("." + currentKey);
   activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed")
+  }, 100);
 }
